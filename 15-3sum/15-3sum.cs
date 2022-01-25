a3b6288c6b973d0d8409bda7af5487f630d1f261
ChinnasamyM <Chinnasamy.Manickam@outlook.com>
2022-01-25 02:34:03 -0600

    Time: 1188 ms (11.13%), Space: 50.6 MB (5.46%) - LeetHub

--- 15-3sum/15-3sum.cs
+++ 15-3sum/15-3sum.cs
@@ -1,109 +1,54 @@
 public class Solution {
-    public IList<IList<int>> ThreeSum(int[] nums) {
-    //We declare the list of lists that will contain our solution
-            IList<IList<int>> result = new List<IList<int>>();
-
-            //The edge case
-            if (nums.Length <= 2) return result;
-            
-            /*For the first, we have to sort the nums*/
-            Array.Sort(nums);
-
-            /*Here we declare 3 indexes. This is how it works. 
-            -4 -2 -3 -1 0 0 0 2 3 10 21
-             s  l                     r 
-             
-             s - start index, l - left index, r - right index */
-            int start = 0, left, right;
-
-            /*The target is that the number we are looking for to be composed out of 2 numbers from our array.
-            for example, if we have the startIndex at -4, we are looking for those two numbers in the given array
-            which, summed up will be the oposite of -4, which is 4, cuz -4 + 4 = 0 (duh) */
-            int target;
-
-            /*The start goes from 0 to length-2 becuse look here
-             -4 -2 -3 -1 0 0 0 2 3 10 21
-                                 s  l  r      */
-            while (start<nums.Length-2)
-            {
-                target = nums[start] * -1;
-                left = start + 1;
-                right = nums.Length - 1;
-
-                /*Now, the start index is fixed and we move the left and right indexes to find those two number
-                which summed up will be the oposite of nums[s]  */
-                while (left < right)
-                {
-                    /*The array is sorted, so if we move to the left the right index, the sum will decrese */
-                    if (nums[left] + nums[right] > target)
-                    {
-                        --right;
-                    }
-
-                    /*Here is the oposite, it the sum of nums[l] and nums[r] is less that what we are looking for,
-                    then we move the left index, which means that the sum will increase due to the sorted array.
-                    the left index will jump to a bigger value */
-                    else if (nums[left] + nums[right] < target)
-                    {
-                        ++left;
-                    }
-                    /*If none of those are true, then it means that nums[l]+nums[r] = our desired value */
-                    else
-                    {
-                        /*Here we create the solution and add it to the list of lists which contains the result. */
-                        List<int> OneSolution = new List<int>() { nums[start], nums[left], nums[right] };
-                        result.Add(OneSolution);
-
-                        /*Now, in order to generate different solutions, we have to jump over
-                        repetitive values in the array.  */
-                        while (left < right && nums[left] == OneSolution[1])
-                            ++left;
-                        while (left < right && nums[right] == OneSolution[2])
-                            --right;
-                    }
-
-                }
-                /*Now we do the same thing to the start index. */
-                int currentStartNumber = nums[start];
-                while (start < nums.Length - 2 && nums[start] == currentStartNumber)
-                    ++start;
-            }
-            return result;
-    }
-    /* // Below solution will fail when multiple diuplicated zeros
+   
+     // Below solution will fail when multiple diuplicated zeros
     //HashSet<int> t2set=new HashSet<int>();
-    public IList<IList<int>> ThreeSum(int[] nums) {
-        int len = nums.Length; //, target=0;
+    public IList<IList<int>> ThreeSum(int[] nums) {        
+        List<IList<int>> res = new List<IList<int>>();
+        int len = nums.Length, target=0;
+        if(len<=2)
+            return res;
+        //Array.Sort(nums);
         //HashSet<int> dups=new HashSet<int>();
         Dictionary<int, int> dups = new Dictionary<int, int>();
-        List<IList<int>> res = new List<IList<int>>();
         Dictionary<int, int> seen = new Dictionary<int, int>();
         //Dictionary<int, int> dd = new HashSet();
+        HashSet<string> tset=new HashSet<string>();
         for(int i=0; i<len; ++i){
             int t=0;
-            if(dups.TryGetValue(nums[i], out t) !=true){ //
-                dups.Add(nums[i], i);
-                for(int j= i+1; j<len; ++j){ // TWO SUM value
-                    int find = -nums[i] - nums[j];
+            if(true || dups.TryGetValue(nums[i], out t) !=true){ //
+                //dups.Add(nums[i], i);
+                for(int j= 0; j<len && i!=j; ++j){ // TWO SUM value
+                    int find = target - nums[i] - nums[j];
                     int temp=0;
-                    if(seen.TryGetValue(find, out temp ) && temp==i){
+                    if(seen.TryGetValue(find, out temp ) && temp !=i && temp !=j){
                         List<int> triplet = new List<int>();
                         triplet.Add(nums[i]);
                         triplet.Add(nums[j]);
-                        triplet.Add(find);
-                       // triplet.Sort();// ??
-                        res.Add(triplet);
+                        triplet.Add(find);                        
+                        triplet.Sort(); //
+                        string str = triplet[0].ToString() + triplet[1].ToString() + triplet[2].ToString();  
+                        //nums[i].ToString() + nums[j].ToString() + find.ToString();
+                        if(!tset.Contains(str)){
+                            res.Add(triplet);
+                            tset.Add(str);
+                        }
                     }                    
                     if(!seen.TryGetValue(nums[j], out temp)){
-                        seen.Add(nums[j], i);
+                        seen.Add(nums[j], j);
                     }
                 }
             }
         }
        
         return res;
-        */
+        
+    }
+    
+    
+    
 }
+
+// perfect working solution
 
  /*
         int i=0, j=1, k=len-1;
