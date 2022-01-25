482c8e587a839f3bfbb951b762a16832e38cf12c
ChinnasamyM <Chinnasamy.Manickam@outlook.com>
2022-01-25 02:42:39 -0600

    Time: 1637 ms (7.59%), Space: 49 MB (11.51%) - LeetHub

--- 15-3sum/15-3sum.cs
+++ 15-3sum/15-3sum.cs
@@ -1,60 +1,35 @@
 public class Solution {
     public IList<IList<int>> ThreeSum(int[] nums) {        
         List<IList<int>> res = new List<IList<int>>();
-        int len = nums.Length, target=0;
+        int len = nums.Length, target=0; // target * -1 is required if target!=0
         if(len<=2)
             return res;
         Dictionary<int, int> seen = new Dictionary<int, int>();
         HashSet<string> tset=new HashSet<string>();
         for(int i=0; i<len; ++i){
-            int t=0;
-                //dups.Add(nums[i], i);
-                for(int j= 0; j<len && i!=j; ++j){ // TWO SUM value
-                    int find = target - nums[i] - nums[j];
+                for(int j= 0; j<len && i!=j; ++j){ // TWO SUM problem concept with found triplet sorted using array.Sort
+                    int find = target - nums[i] - nums[j]; // potental match calc
                     int temp=0;
-                    if(seen.TryGetValue(find, out temp ) && temp !=i && temp !=j){
+                    if(seen.TryGetValue(find, out temp ) && temp !=i && temp !=j){ // retrive it from stored hash value
                         List<int> triplet = new List<int>();
                         triplet.Add(nums[i]);
                         triplet.Add(nums[j]);
                         triplet.Add(find);                        
-                        triplet.Sort(); //
-                        string str = triplet[0].ToString() + triplet[1].ToString() + triplet[2].ToString();  
-                        //nums[i].ToString() + nums[j].ToString() + find.ToString();
+                        triplet.Sort(); // Sorting is required to maintain unique values of each elements-set
+                        string str = triplet[0].ToString() + triplet[1].ToString() + triplet[2].ToString();
                         if(!tset.Contains(str)){
                             res.Add(triplet);
                             tset.Add(str);
                         }
-                    }                    
+                    }  
+                    // as we are into n^2 complexity ist possible same value insert hence avoid them using below dictionary check
                     if(!seen.TryGetValue(nums[j], out temp)){
                         seen.Add(nums[j], j);
                     }
                 }
             
         }
-       
         return res;
-        
-    }
-    
-    
+    }   
     
 }
-
-// perfect working solution
-
- /*
-        int i=0, j=1, k=len-1;
-        while(i<len && k>=0 && j<len && i!=j){
-            string strval= nums[i].ToString()+ nums[j].ToString() + nums[k].ToString();
-            if(i!=j && i!=k && j!=k && j!=i)
-                if(nums[i] + nums[j] + nums[k] ==target && !set.Contains(strval))
-                {
-                   res.Add(new List<int>{nums[i], nums[j], nums[k] });
-                   set.Add(strval); 
-                }
-            //if(j+2<len)
-            j+=2;
-            i++;
-            k--;            
-        }
-        */
