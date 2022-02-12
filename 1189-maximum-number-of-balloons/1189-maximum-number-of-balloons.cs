2a6758933f00494faab65cb4708a6fef8d7fde9a
ChinnasamyM <Chinnasamy.Manickam@outlook.com>
2022-02-12 15:42:06 -0600

    Time: 83 ms (63.78%), Space: 35.5 MB (51.18%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cs
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cs
@@ -1,40 +1,31 @@
 public class Solution {
     public int MaxNumberOfBalloons(string text) {
         string match="balloon";
-       // HashSet<char> set = new HashSet<char>();
         Dictionary<char, int> dict=new Dictionary<char, int>();
         int count=0;
         if(text.Length < match.Length)
             return count;
         int j=0;int _cnt=0;
         for(int i=0; i<text.Length; i++){
-           // Console.Write("i={0} ", text[i]);
             if(match[j]==text[i]){
-                //i++;
                 j++;
             }
             else{
-                //set.Add(text[i]);
                  _cnt=0;
                 if(dict.TryGetValue(text[i], out _cnt)){
                     _cnt +=1;
-                  //  Console.Write("Exists: {0}:{1} , ", text[i], _cnt);
                     dict[text[i]]=_cnt;
                 }
                 else{
                     _cnt=1;
                     dict.Add(text[i], _cnt);
-                }                
-                //i++;
+                }    
             }
             if(j==match.Length){
                 j=0;
                 count+=1;
             }
-           // i++;
         }
-        //Console.WriteLine(j.ToString());
-        //Display(dict);
         
         bool isExit=false;
         while(dict.Count>0 && isExit==false){
@@ -44,7 +35,6 @@ public class Solution {
                     dict.Remove(match[j]);
                 else
                     dict[match[j]] = _cnt-1;
-                //set.Remove(match[j]);
                 j++;
             }
             else
@@ -56,11 +46,5 @@ public class Solution {
         }
         return count;
         
-    }
-    
-    public void Display(Dictionary<char, int> dict){
-        foreach(KeyValuePair<char, int> kv in dict){
-            Console.WriteLine("{0} : {1} ", kv.Key, kv.Value);
-        }
     }
 }
