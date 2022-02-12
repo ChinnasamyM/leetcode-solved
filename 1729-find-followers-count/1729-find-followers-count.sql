68b303cef592b9ac21381406eefc3f8c88f7c3ba
ChinnasamyM <Chinnasamy.Manickam@outlook.com>
2022-02-12 01:41:07 -0600

    Time: 1497 ms (63.42%), Space: 0B (100.00%) - LeetHub

---
+++ 1729-find-followers-count/1729-find-followers-count.sql
@@ -0,0 +1,10 @@
+/* Write your T-SQL query statement below */
+/*
+select u.user_id as user_id , (select count(followers_id) from followers where user_id=u.user_id ) as  followers_count
+
+from followers as u order by u.user_id
+
+*/
+
+select user_id, COUNT(follower_id ) as 'followers_count' from followers 
+group by user_id having count(*)>0
