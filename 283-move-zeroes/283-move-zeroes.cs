48b40d731d825177838943ee468d27814e567204
ChinnasamyM <Chinnasamy.Manickam@outlook.com>
2022-02-13 13:17:59 -0600

    Time: 235 ms (43.25%), Space: 47.1 MB (24.04%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public void MoveZeroes(int[] nums) {
+        int len=nums.Length, i=0, curr=0, numops=0;
+        while(i < len){
+            if(nums[i]==0) 
+                numops++;            
+            if(nums[i] !=0 && numops>0){
+                //Console.Write("Element:{0}, curr:{1} ", nums[i], curr);
+                nums[curr]=nums[i];
+                nums[i] =0;
+                curr++;                
+            }
+            else if(nums[i]!=0 && numops<=0)
+                curr++;
+            i++;
+        }
+        //Console.WriteLine(" {0}, {1}, {2}", i, curr, numops);
+        
+    }
+}
