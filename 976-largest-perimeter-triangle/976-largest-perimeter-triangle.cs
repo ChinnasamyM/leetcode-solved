1f4bf7233878cd7e4700cb36bfd3900a2b022129
ChinnasamyM <Chinnasamy.Manickam@outlook.com>
2022-02-14 00:22:27 -0600

    Time: 219 ms (18.75%), Space: 39.9 MB (50.00%) - LeetHub

--- 976-largest-perimeter-triangle/976-largest-perimeter-triangle.cs
+++ 976-largest-perimeter-triangle/976-largest-perimeter-triangle.cs
@@ -1,6 +1,7 @@
 public class Solution {
     public int LargestPerimeter(int[] nums) {
-        Array.Sort(nums);
+        Array.Sort(nums); 
+        //Per the largest triangle permieter sum of two side is greater than or equal to largest area of other side.
         for(int i=nums.Length-3; i>=0; i--)
             if(nums[i]+nums[i+1] > nums[i+2])
                 return nums[i] + nums[i+1] + nums[i+2];
