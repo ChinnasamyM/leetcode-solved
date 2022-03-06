efc6d0e23b8600f7d54b8039c7835c9d8fb91c13
ChinnasamyM <Chinnasamy.Manickam@outlook.com>
2022-03-06 12:29:18 -0600

    Time: 84 ms (100.00%), Space: 40 MB (6.45%) - LeetHub

--- 1619-mean-of-array-after-removing-some-elements/1619-mean-of-array-after-removing-some-elements.cs
+++ 1619-mean-of-array-after-removing-some-elements/1619-mean-of-array-after-removing-some-elements.cs
@@ -1,13 +1,23 @@
 public class Solution {
-    public double TrimMean(int[] arr) {
-        Array.Sort(arr);
+    public double TrimMean(int[] arr) { 
+        /*Note:
+        1.  Without sorting is makes to store in dictionary and subtract the minumum number elemets and maximum percent number of element by search. in order to avoid complexity in storage and hashing better to sort.
         
-        int i = arr.Length * 5 /100;
-        int j = arr.Length - i;
-        
-        double val = 0;
-        
-        for(int k=i; k < j; k++) {val += arr[k]; }
-        return (i>=j)? 0 : val/(j-i);
+        2. Loop starts from min 5% th poistion ie., from target and ends at len=Array.Length - target.
+        3. If new Length is <= tagrget ie., 5 % value then the sum would come 0.
+        4. Two time of target number of elements will be removed.
+        */
+        Array.Sort(arr);       
+        int target = arr.Length * 5 /100;
+        int len = arr.Length - target;
+        if(target>=len)
+            return 0;
+        double sum = 0;
+        for(int i=target; i < len; i++) {
+          sum += arr[i]; 
+        }
+        int count = arr.Length - (target *2);
+        return sum/count;
+                
     }
 }
