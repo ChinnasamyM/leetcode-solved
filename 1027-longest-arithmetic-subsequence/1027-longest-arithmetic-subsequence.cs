b0eed81f360cd95466f7048870727fffca6dbbfc
ChinnasamyM <Chinnasamy.Manickam@outlook.com>
2022-03-22 00:35:02 -0500

    Time: 568 ms (82.35%), Space: 55 MB (44.12%) - LeetHub

--- 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cs
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cs
@@ -1,63 +1,43 @@
 public class Solution {
     public int LongestArithSeqLength(int[] nums) {
+        int len =nums.Length;
+        if(len<2)
+            return 0;
         
-        
-        int[] A =nums;
-        Dictionary<int,int>[] dp = new Dictionary<int,int>[A.Length];
+        Dictionary<int,int>[] dp = new Dictionary<int,int>[len];
         int ans = 2;
-        for (int j = 0; j < A.Length; j++){
+        for (int j = 0; j < len; j++){
             dp[j] = new Dictionary<int,int>();
             for (int i = 0; i < j; i++){
-                int d = A[j] - A[i];
-                if (dp[i].ContainsKey(d)){
-                    dp[j][d] = dp[i][d]+1;
+                int diff = nums[j] - nums[i];
+                if (dp[i].ContainsKey(diff)){
+                    dp[j][diff] = dp[i][diff]+1;
                 }
                 else {
-                    dp[j][d] = 2;
+                    dp[j][diff] = 2;
                 }
-                ans = Math.Max(ans, dp[j][d]);
+                ans = Math.Max(ans, dp[j][diff]);
             }
         }
         return ans;
         /*
-                int len = nums.Length;
-        if(len<2)
-            return 0;
-        
-        Dictionary<int, List<int>> _ = new Dictionary<int, List<int>>();
-        Array.Sort(nums);
+        // Below sln will mfail with these pattern as we can calc per differemce pattern alone... [83,20,17,43,52,78,68,45] 
+        // To make it work we need to add AP-subsequence validation and return the successfull max one.
+        Dictionary<int, List<int>> _dt = new Dictionary<int, List<int>>();
         Dictionary<int, int> dict = new Dictionary<int, int>();
-        int max=Int32.MinValue, num=0;
+        int max=2;
         for(int i=0; i<len; i++){
-            for(int j=i+1; j<len; j++){ //len && i!=j
-                int diff = nums[j] - nums[i]; //Math.Abs(nums[j] - nums[i] );
-                int val=0;
-                if(dict.TryGetValue(diff, out val)){
-                    dict[diff] +=1;
-                    
-                    List<int> __ = _[diff];
-                    __.Add(nums[j]);
-                    _[diff] = __;
-                    
+            for(int j=0; j<i; j++){ 
+                int diff = nums[i] - nums[j]; 
+                if(dict.ContainsKey(diff)){
+                    dict[diff] +=1;                    
                 }
                 else{
                     dict.Add(diff, 2);
-                    
-                    List<int> t_ =new List<int>();                    
-                    t_.Add(nums[i]);
-                    t_.Add(nums[j]);
-                    _.Add(diff, t_);
                 }
                 max = Math.Max(max, dict[diff]);
             }
-            
         }
-        foreach(KeyValuePair<int, List<int>> kv in _ ){
-            for(int i=0; i<kv.Value.Count; i++)
-                Console.Write("{0}, ", kv.Value[i]);
-            Console.WriteLine(" -> {0}", kv.Key);
-        }
-        //Console.WriteLine("num={0}, max={1}", num, max);
         return max;
         */
             
